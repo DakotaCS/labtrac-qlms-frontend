17b4142a726149ff78a6e17c5f19c53b3bd8e030
Dakota Soares <dakota.soares@execulink.com>
2024-09-09 18:25:12 -0400

    Layout and Side Menu Tweaks

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,12 @@
-// ./components/Footer.tsx
-
 import React from 'react';
+import './Footer.css'; // Import the CSS file
 
 const Footer: React.FC = () => {
   return (
-    <footer style={footerStyle}>
+    <footer className="footer">
       <p>&copy; 2024 Quantus Engineering</p>
     </footer>
   );
-};
-
-const footerStyle = {
-  backgroundColor: '#282c34',
-  padding: '10px',
-  color: 'white',
-  textAlign: 'center' as const,
-  position: 'fixed' as const,
-  bottom: 0,
-  width: '100%',
 };
 
 export default Footer;
