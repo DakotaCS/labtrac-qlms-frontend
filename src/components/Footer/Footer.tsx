e06852d8f383a9d25373832c6db1aea4a4869eb5
Dakota Soares <dakota.soares@execulink.com>
2024-11-15 12:04:20 -0500

    Update Footer and Remove Login Link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
-import './Footer.css'; // Import the CSS file
+import './Footer.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-      <Link to="/logout" className="logout-link">Logout</Link>
-        <p className="footer-text">&copy; 2024 Quantus Engineering</p>
+        <p className="footer-text">&copy; 2024 DKS Technical Solutions</p>
       </div>
     </footer>
   );
